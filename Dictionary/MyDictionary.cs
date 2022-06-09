b74dbd7151d1b668be4fb78c2e7bf5eeb1d62fad
amach <amach@DESKTOP-9BAKMLG>
2022-06-09 22:52:52 +0400

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Internal.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private struct Entry
+        {
+            public int hashCode;
+            public int next;
+            public TKey key;
+            public TValue value;
+        }
+        private int[] buckets;
+        private Entry[] entries;
+        private int count;
+        private int version;
+        private int freeList;
+        private int freeCount;
+        private IEqualityComparer<TKey> comparer;
+        internal const Int32 HashPrime = 101;
+
+        public MyDictionary(IDictionary<TKey, TValue> dictionary)
+        {
+
+        }
+        public MyDictionary()
+        {
+
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Insert(key, value, true);
+        }
+
+        private void Insert(TKey key, TValue value, bool add)
+        {
+
+            if (key == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (buckets == null) Initialize(0);
+            int hashCode = comparer.GetHashCode(key) & 0x7FFFFFFF;
+            int targetBucket = hashCode % buckets.Length;
+        }
+        private void Initialize(int capacity)
+        {
+            int size = GetPrime(capacity);
+            buckets = new int[size];
+            for (int i = 0; i < buckets.Length; i++) buckets[i] = -1;
+            entries = new Entry[size];
+            freeList = -1;
+        }
+        public static readonly int[] primes = {
+            3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
+            1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
+            17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
+            187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
+            1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369};
+
+        public static int GetPrime(int min)
+        {
+            for (int i = 0; i < primes.Length; i++)
+            {
+                int prime = primes[i];
+                if (prime >= min) return prime;
+            }
+            for (int i = (min | 1); i < Int32.MaxValue; i += 2)
+            {
+                if (IsPrime(i) && ((i - 1) % HashPrime != 0))
+                    return i;
+            }
+            return min;
+        }
+        public static bool IsPrime(int candidate)
+        {
+            if ((candidate & 1) != 0)
+            {
+                int limit = (int)Math.Sqrt(candidate);
+                for (int divisor = 3; divisor <= limit; divisor += 2)
+                {
+                    if ((candidate % divisor) == 0)
+                        return false;
+                }
+                return true;
+            }
+            return (candidate == 2);
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> myDictionary = new MyDictionary<string,int>();
+            myDictionary.Add("test1", 1);
+            myDictionary.Add("test2", 2);
+            Console.WriteLine(myDictionary);
+            Console.ReadLine();
+        }
+    }
+}
